05c6b155babd473a45f2fb14f9eca55d36f14645
kong33 <kge021114@naver.com>
2024-03-02 03:01:57 +0900

    feat : option과 options  컴포넌트 제작 및 스타일링

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -1,16 +1,39 @@
 import OptionPicker from './OptionPicker';
-function Option() {
-  const colorChart = [
-    $color - orange - 200,
-    $color - purple - 200,
-    $color - blue - 200,
-    $color - green - 200,
-  ];
+import styles from './Option.module.scss';
+import useFetch from '../../hooks/useFetch';
+import { useState } from 'react';
+
+function Option({ type }) {
+  //type은 image, color중 하나
+  const colorChart = ['orange', 'purple', 'blue', 'green'];
+  const backgroundImgChart = useFetch(`/background-images/`).imageUrls;
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionClicked = (option) => {
+    setSelectedOption(option);
+  };
   return (
-    <div>
-      {colorChart.map((color) => (
-        <OptionPicker color={color} />
-      ))}
+    <div className={styles.options}>
+      {type === 'color' &&
+        colorChart?.map((color) => (
+          <OptionPicker
+            color={color}
+            key={color}
+            isSelected={selectedOption === color}
+            onOptionClick={() => handleOptionClicked(color)}
+          />
+        ))}
+      {type === 'image' &&
+        backgroundImgChart?.map((img) => (
+          <OptionPicker
+            backgroundImg={img}
+            key={img}
+            isSelected={selectedOption === img}
+            onOptionClick={() => handleOptionClicked(img)}
+          />
+        ))}
     </div>
   );
 }
+
+export default Option;
--- src/components/Option/OptionPicker.jsx
+++ src/components/Option/OptionPicker.jsx
@@ -1,9 +1,57 @@
 import styles from './Option.module.scss';
+import Check from '../../assets/images/Check';
+import { useState } from 'react';
 
-function OptionPicker({ color }) {
+function OptionPicker({
+  color = 'white',
+  backgroundImg = null,
+  onOptionClick,
+  isSelected,
+}) {
+  const [isHovered, setIsHovered] = useState(false);
+
   const style = {
-    backgroundColor: color,
+    backgroundImage: `url(${backgroundImg})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
   };
-  return <div className={styles.div} style={style}></div>;
+
+  const handleClick = () => {
+    if (!isSelected) {
+      onOptionClick();
+    }
+  };
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  return (
+    <div
+      className={styles.optionPicker}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={`${styles.checkWrapper} ${styles[color]} `}
+        onClick={handleClick}
+        style={backgroundImg ? style : {}}
+      ></div>
+      <div
+        className={
+          isSelected
+            ? styles.checkClicked
+            : isHovered
+              ? styles.checkHover
+              : styles.check
+        }
+      >
+        {(isSelected || isHovered) && <Check />}
+      </div>
+    </div>
+  );
 }
 export default OptionPicker;
